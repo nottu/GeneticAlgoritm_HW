699b4de5591b3138dd68f25ceb0ac26c5816c6ea
Javier Peralta <nottu1990@gmail.com>
2018-04-06 01:57:42 -0500

    take params for functions from cmd

--- main.cpp
+++ main.cpp
@@ -2,19 +2,27 @@
 #include "GeneticAlgorithm.hpp"
 #include <cmath>
 #include <chrono>
+#include <string>
 
 using namespace std;
 
 int main(int argc, const char **argv) {
+  if(argc < 4){
+    cout << "gene [func_name] [min] [max]";
+    exit(1);
+  }
   srand(chrono::system_clock::now().time_since_epoch().count());
-//  auto problem = OptiProblem(sphere, -600.0, 600.0);
-//  auto problem = OptiProblem(sphere, -20.0, 20.0);
-//   auto problem = OptiProblem(zakharov, -20.0, 20.0);
+  string func = argv[1];
+  OptiFunction opt = sphere;
+  if(func == "ellipsoid")     opt = ellipsoid;
+  else if(func == "zakharov") opt = zakharov;
 
-//  auto problem = OptiProblem(rosenbrock, -20.0, 20.0);
-//  auto problem = OptiProblem(ackley, -20.0, 20.0);
-//  auto problem = OptiProblem(griewank, -600.0, 600.0);
-  auto problem = OptiProblem(rastrigin, -20.0, 20.0);
+  else if(func == "rosenbrock") opt = rosenbrock;
+  else if(func == "ackley")     opt = ackley;
+  else if(func == "griewank")   opt = griewank;
+  else if(func == "rastrigin")  opt = rastrigin;
+
+  auto problem = OptiProblem(opt, atoi(argv[2]), atoi(argv[3]));
   for (int i = 0; i < 1; ++i) {
     auto algo = GeneticAlgorithm(1000, 300 * 1000, problem);
     while(algo.iterate(100, 10)){}
